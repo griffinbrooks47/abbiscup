c6a7cc0ef81a5e30ffbe9c7ef37f63607924f542
gbrooks <griffinbrooks47@gmail.com>
2024-07-20 16:18:50 -0500

    text

--- app/page.tsx
+++ app/page.tsx
@@ -85,10 +85,10 @@ export default function Home() {
         </div>
       </section>
       <section className="section-div flex m-auto bg-[#F5F5F5]">
-        <div className="section-container flex justify-center w-full">
-          <div className="text-container flex flex-col justify-center">
+        <div className="section-container flex justify-center items-center w-full">
+          <div className="text-container flex flex-col justify-center items-center">
             <div className="opacity-[90%] mb-[1.5rem]">
-              <p className="pt-[0rem]">Although the depth of her illness wasn&apos;t always evident, she struggled for many years with OCD, which in turn created high levels of anxiety and eventually depression. Her condition was not responsive to traditional therapies and treatments and became more debilitating and intrusive. Abbi faced every day with a relentless disease for as long as she could before her suffering ended at the age of 16 on March 5, 2024.</p>
+              <p className="pt-[0] md:pt-[7.5rem]">Although the depth of her illness wasn&apos;t always evident, she struggled for many years with OCD, which in turn created high levels of anxiety and eventually depression. Her condition was not responsive to traditional therapies and treatments and became more debilitating and intrusive. Abbi faced every day with a relentless disease for as long as she could before her suffering ended at the age of 16 on March 5, 2024.</p>
             </div>
             <hr className="w-[100%] mb-[1.5rem] border-t-[0.5px] border-[rgba(0,0,0,0.2)]"></hr>
           </div>
